0da53df08ce707d313ab69f8cdbefa9f8cf7c0e0
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-08-15 19:38:16 +0200

    Add user model and create basic project structure

--- app/src/main/java/com/sipox11/android_dagger2_demo/ui/MainActivity.java
+++ app/src/main/java/com/sipox11/android_dagger2_demo/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.sipox11.android_dagger2_demo;
+package com.sipox11.android_dagger2_demo.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.sipox11.android_dagger2_demo.R;
 
 public class MainActivity extends AppCompatActivity {
 
