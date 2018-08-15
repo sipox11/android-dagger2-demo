8d458eb50f70ba526d20b02dd66224495df6338d
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-08-15 19:41:04 +0200

    Setup custom qualifiers and scopes for dagger to use

---
+++ app/src/main/java/com/sipox11/android_dagger2_demo/config/qualifiers/DatabaseInfo.java
@@ -0,0 +1,11 @@
+package com.sipox11.android_dagger2_demo.config.qualifiers;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DatabaseInfo {
+}
---
+++ app/src/main/java/com/sipox11/android_dagger2_demo/config/scopes/ActivityScope.java
@@ -0,0 +1,11 @@
+package com.sipox11.android_dagger2_demo.config.scopes;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActivityScope {
+}
